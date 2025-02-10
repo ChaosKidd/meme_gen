bad7083c44272962ca88dd34bc1f4c3c47dfa3d1
CootsAndLoons <cootsgrebesloons@gmail.com>
2025-02-10 22:20:39 +0200

    project structure

---
+++ Meme_gen/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Meme_gen.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
