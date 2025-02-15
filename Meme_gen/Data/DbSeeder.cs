2c9e18b29293ddfd04ba50d38412a746adc3d11e
kriskata06 <kbs_teen16@abv.bg>
2025-02-15 23:55:28 +0200

    Ni i dont think i will find love

--- Meme_gen/Data/DbSeeder.cs
+++ Meme_gen/Data/DbSeeder.cs
@@ -10,56 +10,20 @@ namespace meme_gen.Data
         {
             if (!context.Memes.Any())  // Only seed if database is empty
             {
-                context.Memes.AddRange(
-                    new MemeTemplate
+                var memes = new List<MemeTemplate>();
+
+                for (int i = 1; i <= 38; i++)
+                {
+                    memes.Add(new MemeTemplate
                     {
-                        ImagePath = "/images/meme_1.jpeg",
+                        ImagePath = $"/images/meme_{i}.jpg",
                         DateAdded = DateTime.Now
-                    },
-                    new MemeTemplate
-                    {
-                        ImagePath = "/images/meme_2.jpeg",
-                        DateAdded = DateTime.Now
-                    },
-                    new MemeTemplate
-                    {
-                        ImagePath = "/images/meme_3.jpg",
-                        DateAdded = DateTime.Now
-                    },
-                    new MemeTemplate
-                    {
-                        ImagePath = "/images/meme_4.jpg",
-                        DateAdded = DateTime.Now
-                    },
-                    new MemeTemplate
-                    {
-                        ImagePath = "/images/meme_5.jpg",
-                        DateAdded = DateTime.Now
-                    },
-                    new MemeTemplate
-                    {
-                        ImagePath = "/images/meme_6.jpg",
-                        DateAdded = DateTime.Now
-                    },
-                    new MemeTemplate
-                    {
-                        ImagePath = "/images/meme_7.jpg",
-                        DateAdded = DateTime.Now
-                    },
-                    new MemeTemplate
-                    {
-                        ImagePath = "/images/meme_8.jpg",
-                        DateAdded = DateTime.Now
-                    },
-                    new MemeTemplate
-                    {
-                        ImagePath = "/images/meme_9.jpg",
-                        DateAdded = DateTime.Now
-                    }
-                );
+                    });
+                }
+
+                context.Memes.AddRange(memes);
                 context.SaveChanges();
             }
         }
     }
-
 }
